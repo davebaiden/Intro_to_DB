f8091fd3d516e5ff112986262ac80b789e38af9c
David Baiden Amissah <davidbamissah@gmail.com>
2025-08-17 23:31:43 +0000

    Updates

--- task_4.sql
+++ task_4.sql
@@ -1,7 +1,12 @@
 -- task_4.sql
 
--- Use the correct database
-USE alx_book_store;
-
--- Print full description of the Books table
-DESCRIBE Books;
+SELECT 
+    COLUMN_NAME AS 'Column Name',
+    COLUMN_TYPE AS 'Data Type',
+    IS_NULLABLE AS 'Nullable',
+    COLUMN_KEY AS 'Key',
+    COLUMN_DEFAULT AS 'Default',
+    EXTRA AS 'Extra'
+FROM INFORMATION_SCHEMA.COLUMNS
+WHERE TABLE_SCHEMA = 'alx_book_store'
+  AND TABLE_NAME = 'Books';
